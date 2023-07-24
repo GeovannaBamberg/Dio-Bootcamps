951c7a8072ac120663c59050a95e72357b059f2f
Geovanna Bamberg <geovanna.t.bamberg@gmail.com>
2023-07-24 16:52:49 -0300

    atualizacao

--- orange/pokedex/assets/js/main.js
+++ orange/pokedex/assets/js/main.js
@@ -1,6 +1,22 @@
-const buttaoGerador = document.querySelector('h1')
-buttaoGerador.innerText+=""
+const offset = 0
+const limit = 10
+const url = `https://pokeapi.co/api/v2/pokemon/?offset=${offset}&limit=${limit}`
 
-function geraPokemon(){
-    alert('oi')
+fetch(url)
+.then((response) => response.json())
+ /*   .then(function(responseBody) {
+        console.log(responseBody)
+    })*/
+.then((jsonbody)=> console.log(jsonbody))
+.catch((error)=> console.log(error))
+.finally(() => console.log('Requisição Concluida!'))
+
+/* try{
+
+}catch{
+
+}finally{
+
 }
+*/
+
